60b7b340f76eee5689b98654c1d0c2ad01e44408
Balaj Marius <balajmarius93@gmail.com>
2018-04-11 17:14:09 +0300

    refactor

--- index.js
+++ index.js
@@ -1,154 +1,16 @@
-const Svgo = require('svgo')
-const cheerio = require('cheerio')
-const camelCase = require('camel-case')
-const transform = require('css-to-react-native')
+const { optimize } = require('./lib/optimizer')
+const { parse } = require('./lib/parser')
+const { transform } = require('./lib/transformer')
 
-/**
- * Config
- */
+const svg = `
+<svg id="Layer_1" class="test-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <g x="5" fill="blue">
+    <path style="fill: red;margin-left: 10px;padding-right:20px" class="cls-20" d="M27.13,75h19.5l-5-15.5H23.5a1,1,0,0,0-.95,1.31Z"/>
+  </g>
+</svg>
+`
 
-const CONFIG = {
-  plugins: [
-    { removeScriptElement: true },
-    { removeStyleElement: true },
-    { convertShapeToPath: false },
-    { removeHiddenElems: false },
-    { convertPathData: false },
-    { mergePaths: false },
-    { cleanupIDs: false },
-    { removeTitle: true },
-    { removeDesc: true },
-  ]
-}
-
-
-/**
- * Constants
- */
-
-const JSX_ATTRIBUTES = {
-  class: 'className',
-  viewbox: 'viewBox',
-  linearGradient: 'linearGradient',
-  gradientTransform: 'gradientTransform'
-}
-
-const REGEX = {
-  QUOTE: /\"/g,
-  WHITE_SPACE: /\s/g,
-  JSX: /"({{)|(}})"/g,
-}
-
-const INLINE_STYLE = 'style'
-const EMPTY_STRING = ''
-
-/**
- * Parse CSS
- * @returns {Array}
- */
-
-const parseCss = cssString => {
-
-  const trimmedCss = cssString.replace(REGEX.WHITE_SPACE, EMPTY_STRING)
-  const parsedCss = trimmedCss.split(';').map(proprieties => proprieties.split(':'))
-
-  return parsedCss.filter(proprieties => proprieties.length > 1)
-
-}
-
-/**
- * Reactify CSS
- * @returns {Object}
- */
-
-const transformCss = cssString => {
-
-  const parsedCss = parseCss(cssString)
-  const transformedCss = transform.default(parsedCss)
-  const proprieties = Object.keys(transformedCss)
-
-  const reactCss = proprieties.reduce((accumulator, proprietyName, index) => {
-
-    const value = transformedCss[proprietyName]
-    const separator = (index < proprieties.length-1) ? ',' : EMPTY_STRING
-
-    return accumulator += `${proprietyName}:${JSON.stringify(value)}${separator}`
-
-  }, EMPTY_STRING)
-
-  return `{{${reactCss}}}`
-
-}
-
-/**
- * SVG {String}
- * @returns optimized SVG {String}
- */
-
-const optimize = svgString => {
-
-  const svgo = new Svgo(CONFIG)
-
-  return new Promise((resolve, reject) => svgo.optimize(svgString, response => {
-
-    const { error, data } = response
-
-    if (error) reject(error)
-
-    resolve(data)
-
-  }))
-
-}
-
-/**
- * SVG {String}
- * @returns SVG with camel case attributes {String}
- */
-
-const jsxify = svgString => {
-
-  const parsedHtml = cheerio.load(svgString, { decodeEntities: false })
-  const elements = parsedHtml('*')
-
-  elements.each(function() {
-
-    const currentElement = parsedHtml(this)
-    const attributes = currentElement.attr()
-
-    for (let attribute in attributes) {
-
-      let attributeValue = attributes[attribute]
-      let jsxAttribute = camelCase(attribute)
-
-      if (JSX_ATTRIBUTES[attribute])
-        jsxAttribute = JSX_ATTRIBUTES[attribute]
-
-      if (attribute === INLINE_STYLE)
-        attributeValue = transformCss(attributeValue)
-
-      currentElement
-        .removeAttr(attribute)
-        .attr(jsxAttribute, attributeValue)
-
-    }
-
-  })
-
-  const transformedSvg = parsedHtml.html()
-
-  // replace quotes before and after {{}}
-  return transformedSvg.replace(REGEX.JSX, '$1$2')
-
-}
-
-/**
- * SVG {String}
- * @returns JSX valid SVG {String}
- */
-
-const svgToJsx = svgString => optimize(svgString)
-  .then(jsxify)
-
-
-module.exports = svgToJsx
+optimize(svg)
+  .then(parse)
+  .then(transform)
+  .then(console.log)
