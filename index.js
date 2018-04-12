c3c4837d87d14853ca89458c9116a0065219a8b3
Balaj Marius <balajmarius93@gmail.com>
2018-04-12 13:27:17 +0300

    replaced parser

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const { parse } = require('./lib/parser')
 const { transform } = require('./lib/transformer')
 
 optimize(`
-  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
-    <g>
-    <path style="padding-right:30px;" d="M50.049,0.3c14.18,0.332,25.969,5.307,35.366,14.923S99.675,36.9,100,51.409c-0.195,11.445-3.415,21.494-9.658,30.146 - yadda yadda yadda"/>
-    </g>
+  <svg version="1.1" id="Layer_1" class="test" style="padding-right: 10px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+    <defs>
+      <linearGradient id="linear-gradient" x1="293.33" y1="67.32" x2="81.07" y2="279.59" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#141414" stop-opacity="0"/>
+        <stop offset="1" stop-color="#141414"/>
+      </linearGradient>
+      <linearGradient id="linear-gradient-2" x1="180.64" y1="340.02" x2="180.64" y2="248.55" xlink:href="#linear-gradient"/>
+      <linearGradient id="linear-gradient-3" x1="168.65" y1="220.02" x2="168.65" y2="206.04" xlink:href="#linear-gradient"/>
+    </defs>
   </svg>`)
   .then(parse)
   .then(transform)
